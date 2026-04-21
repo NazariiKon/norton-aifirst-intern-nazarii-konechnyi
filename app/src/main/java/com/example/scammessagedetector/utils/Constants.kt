a67f13270db34edc4d3701f4fb869a6fcda2520e
Nazarii Konechnyi <nazar.konechniy2@gmail.com>
2026-04-21 16:48:25 +0100

    utils directory

---
+++ app/src/main/java/com/example/scammessagedetector/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.scammessagedetector.utils
+
+// Configuration constants for Groq API
+object Constants {
+    // Groq API credentials
+    const val GROQ_MODEL = "llama-3.1-8b-instant"
+}
